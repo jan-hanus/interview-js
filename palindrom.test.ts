9d66733a3f46a6d89ffd1eae5d3a3ca914ad787b
jahanus <hanus.j@icloud.com>
2022-12-19 10:14:14 +0100

    Initial

---
+++ palindrom.test.ts
@@ -0,0 +1,8 @@
+import {isPalindrom} from "./palindrom";
+
+
+describe("Palindrom test", () => {
+    it("", () => {
+        expect(isPalindrom("aba")).toEqual(true)
+    })
+})
---
+++ palindrom.ts
@@ -0,0 +1,6 @@
+
+
+export const isPalindrom = (input: string): Boolean => {
+    // TODO
+    return true;
+}
